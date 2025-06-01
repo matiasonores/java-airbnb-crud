a8de9280eae53ba19d309449cb9a985bef0f5339
Matias Onores <matiasonores@integracomex.com.ar>
2025-06-01 19:28:42 -0300

    First commit
    Creacion del proyecto con Spring Initializr

---
+++ src/main/java/com/airbnb/java_airbnb_crud/JavaAirbnbCrudApplication.java
@@ -0,0 +1,13 @@
+package com.airbnb.java_airbnb_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAirbnbCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAirbnbCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airbnb/java_airbnb_crud/JavaAirbnbCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.java_airbnb_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAirbnbCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
