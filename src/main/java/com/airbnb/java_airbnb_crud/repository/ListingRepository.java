6484243f6f4df2d75fb5778963c089eaee1c74a8
Matias Onores <matiasonores@integracomex.com.ar>
2025-06-01 21:49:34 -0300

    ListingRepository
    Implementacion del repositorio del modelo de Listings

--- src/main/java/com/airbnb/java_airbnb_crud/repository/ListingRepository.java
+++ src/main/java/com/airbnb/java_airbnb_crud/repository/ListingRepository.java
@@ -6,5 +6,23 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ListingRepository extends MongoRepository<Listing, String> {
+    
+    // Para buscar un listing por su id
+    Optional<Listing> findById(String id);
+
+    // Para buscar todos los listings
+    List<Listing> findAll();
+
+    // Create o Update de un listing
+    <S extends Listing> S save(S listing);
+
+    // Para hacer el delete de un listing
+    void deleteById(String id);
+
+    // Para validar que exista un listing segun su id
+    boolean existsById(String id);
+
+    // Para buscar listings por nombre
+    List<Listing> findByNameContainingIgnoreCase(String name);
 
 }
