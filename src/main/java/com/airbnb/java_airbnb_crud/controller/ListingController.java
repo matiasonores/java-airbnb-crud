9b70a4c9d73a06842a46303c532409c15a0a37b3
Matias Onores <matiasonores@integracomex.com.ar>
2025-06-01 21:53:42 -0300

    ListingController
    Implementacion del controller para poder manejar las solicitudes a la bd

--- src/main/java/com/airbnb/java_airbnb_crud/controller/ListingController.java
+++ src/main/java/com/airbnb/java_airbnb_crud/controller/ListingController.java
@@ -13,5 +13,53 @@ import java.util.Optional;
 @RequestMapping("/api/listings")
 public class ListingController {
 
-    
+    @Autowired
+    private ListingService listingService;
+
+    // GET /api/listings  - Get all
+    @GetMapping
+    public List<Listing> getAllListings() {
+        return listingService.getAllListings();
+    }
+
+    // GET /api/listings/{id} - Get por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Listing> getListingById(@PathVariable String id) {
+        Optional<Listing> listing = listingService.getListingById(id);
+        return listing.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // POST /api/listings - Create new
+    @PostMapping
+    public Listing createListing(@RequestBody Listing listing) {
+        return listingService.saveListing(listing);
+    }
+
+    // PUT /api/listings/{id} - Update one
+    @PutMapping("/{id}")
+    public ResponseEntity<Listing> updateListing(@PathVariable String id, @RequestBody Listing listingDetails) {
+        Optional<Listing> existingListing = listingService.getListingById(id);
+        if (existingListing.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Listing listing = existingListing.get();
+
+        listing.setName(listingDetails.getName());
+        listing.setSummary(listingDetails.getSummary());
+        /*
+         * TODO agregar los otros campos
+         */
+
+
+        Listing updatedListing = listingService.saveListing(listing);
+        return ResponseEntity.ok(updatedListing);
+    }
+
+    // DELETE /api/listings/{id} - Delete one
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteListing(@PathVariable String id) {
+        listingService.deleteListing(id);
+        return ResponseEntity.noContent().build();
+    }
 }
